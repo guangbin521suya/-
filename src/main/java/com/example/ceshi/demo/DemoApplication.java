3c67cb9b988ea0d59d0eca67eb6d96f1a345ee67
guangbin521suya <372901830@qq.com>
2020-09-01 14:27:04 +0800

    第一次用git

---
+++ src/main/java/com/example/ceshi/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.ceshi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.print("==启动成功==");
+    }
+
+    @Bean
+    public CacheKeyGenerator cacheKeyGenerator() {
+        return new LockKeyGenerator();
+    }
+
+}
